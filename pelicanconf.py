89f23fecd344ff2de8e512ff7e01e37f3a240184
stdrickforce <stdrickforce@gmail.com>
2017-02-14 16:04:17 +0800

    pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+# -*- coding: utf-8 -*-
+
+# Author: stdrickforce (Tengyuan Fan)
+# Email: <stdrickforce@gmail.com> <fantengyuan@baixing.com>
+
 from __future__ import unicode_literals
 
-AUTHOR = u'\u98ce\u4e4b\u5c0f\u7948'
-SITENAME = u'\u5c0f\u7948\u548c\u5c0f\u7ffc\u7684\u50a8\u85cf\u5ba4'
+AUTHOR = u"Terence"
+SITENAME = u'Inori'
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = u'zh'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,17 +23,28 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = '/Users/stdrickforce/pelican-themes/Flex'
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('You can add links in your config file', '#'),
+    ('Another social link', '#'),
+)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+MAIN_MENU = True
+
+SITETITLE = 'Terrence'
+SITESUBTITLE = 'Web Developer'
+SITELOGO = SITEURL + '/extra/inori.jpeg'
